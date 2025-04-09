58715f8bceb14a1c24f5fc1c2f64b112cb47c35e
yuta-yahagi <yuta.yahagi.public@gmail.com>
2025-04-09 19:06:51 +0900

    Update interface

--- src/vca_tool/vcapp.py
+++ src/vca_tool/vcapp.py
@@ -1,43 +1,34 @@
 # -*- coding: utf-8 -*-
 from subprocess import run
 from pathlib import Path
-import sys, re
+import sys, os, re
 import click
 
-def make_vcapp(ppa, ppb, x:float, dest=None, path_virtual=None, precision=3, location=True):
-
-    if dest is None:
-        dest=Path.cwd()
-    else:
-        dest=Path(dest)
+def make_vcapp(ppa, ppb, x:float, fname=None, path_virtual=None):
 
     # Get Path to "virtual_v2.x"
-    if path_virtual is None:
-        # path_virtual=Path(PATH_VIRTUAL)
+    if path_virtual is not None:
+        path_virtual=Path(path_virtual)
+    elif os.getenv("PATH_VIRTUAL") is not None:
+        # Read from environment variable
+        path_virtual = Path(os.getenv("PATH_VIRTUAL"))
+    else:
+        # Try to find virtual_v2.x in the system path
         try:
             path_virtual=run("which virtual_v2.x",shell=True, capture_output=True,text=True).stdout.strip()
         except:
             raise FileNotFoundError(f"virtual_v2.x not found. Please specify the path to virtual_v2.x")
-        path_virtual=Path(path_virtual)
-    else:
-        path_virtual=Path(path_virtual)
-
     if not path_virtual.exists():
-        raise FileNotFoundError(f"virtual_v2.x not found at {path_virtual.absolute()}")
-    if location:
-        # Because virtual_v2.x is sometime broken with absolute path, we convert it to relative path
-        path_a=Path(ppa).absolute()
-        path_b=Path(ppb).absolute()
-        if not path_a.exists():
-            raise FileNotFoundError(f"PP {path_a.absolute()} not found")
-        if not path_b.exists():
-            raise FileNotFoundError(f"PP {path_b.absolute()} not found")
-                # Because virtual_v2.x is sometime broken with absolute path, we convert it to relative path
-        path_a=Path(ppa).absolute()
-        path_b=Path(ppb).absolute()
-    else:
-        path_a=str(ppa)
-        path_b=str(ppb)
+        raise FileNotFoundError(f"Path to virtual_v2.x {path_virtual.absolute()} not found")
+    
+    path_a=Path(ppa).absolute()
+    path_b=Path(ppb).absolute()
+        
+    if not path_a.exists():
+        raise FileNotFoundError(f"PP {path_a.absolute()} not found")
+    if not path_b.exists():
+        raise FileNotFoundError(f"PP {path_b.absolute()} not found")
+
 
     if x<=0.0 or x>1:
         raise ValueError(f"x value must be in (0:1]")
@@ -55,16 +46,23 @@ def make_vcapp(ppa, ppb, x:float, dest=None, path_virtual=None, precision=3, loc
         raise ValueError(f"Invalid PP name {ppb}")
     elem_b = elem_b.group(1)
 
+
+
     # run(["echo",ppa,ppb,x])
     created_name="NewPseudo.UPF"
-    new_pp_name=f"{elem_a}_{elem_b}_{x}.UPF"
 
-    # cwd=Path.cwd().absolute()
-
-    if dest.is_dir():
-        new_pp_name=str(dest.absolute())+"/"+new_pp_name
-        # created_name=str(dest.absolute())+"/"+created_name
-
+    if fname is None:
+        fname=Path.cwd()
+    if Path(fname).is_dir():
+        dest = fname
+        fname=fname + f"/{elem_a}_{elem_b}_{x}.UPF"
+    else:
+        dest = Path(fname).parent
+        fname=fname.replace("$A", elem_a)
+        fname=fname.replace("$B", elem_b)
+        fname=fname.replace("$x", x)
+        if not fname.endswith(".UPF"):
+            fname=fname + ".UPF"
     # With VCA, for the reason of interpolation, mesh of PP_A should be larger than that of PP_B
     # NOT TRUE: It seems depending on a machine(?!) 
 
@@ -78,22 +76,25 @@ def make_vcapp(ppa, ppb, x:float, dest=None, path_virtual=None, precision=3, loc
     # Just run it twice
     try:
         command1 = f'echo "{path_a}\n{path_b}\n{x}" | {path_virtual.absolute()}'
+        print(f"Running command: {command1}")
         virtual_out=run(command1, shell=True, cwd=dest, capture_output=True, text=True).stdout
         inspect_virtual(virtual_out)
     except:
         command1 = f'echo "{path_b}\n{path_a}\n{y}" | {path_virtual.absolute()}'
+        print("Failed. Try again.")
+        print(f"Running command: {command1}")
         virtual_out=run(command1, shell=True, cwd=dest, capture_output=True, text=True).stdout
         inspect_virtual(virtual_out)
 
     # print(f"Run command:\n {command1}")
 
-    command2 = f'mv {created_name} {new_pp_name}'
+    command2 = f'mv {created_name} {fname}'
     # Because UpfData in AiiDA does not accept "Xx" as element symbol,
     # we replace it with the symbol of element A
-    command3 = f'sed -i -e "s/Xx/{elem_a}/g" {new_pp_name}'
+    command3 = f'sed -i -e "s/Xx/{elem_a}/g" {fname}'
     run(command2, shell=True, cwd=dest)
     run(command3, shell=True,cwd=dest)
-    return new_pp_name
+    return fname
 
 def extract_mesh_size(pp_path):
     with open(pp_path, 'r') as f:
@@ -123,11 +124,11 @@ def inspect_virtual(out):
 @click.argument('ppa')
 @click.argument('ppb')
 @click.argument('x', type=float)
-@click.option('--dest', '-o', default=None, help="Destination directory")
-@click.option('--virtual', '-v', default=None, help="Path to virtual_v2.x")
-@click.option('--precision', '-p', default=3, help="Precision of x value")
-@click.option('--location/--name', '-l/-n', help="Use location (path) to PP or name of it", default=True)
-def main(ppa,ppb,x, dest, virtual, precision, location):
+@click.option('--file', '-f', default=None, help="Output file or directory")
+# @click.option('--dest', '-o', default=None, help="Destination directory")
+@click.option('--path_virtual', '-v', default=None, help="Path to virtual_v2.x")
+# @click.option('--location/--name', '-l/-n', help="Use location (path) to PP or name of it", default=True)
+def main(ppa,ppb,x, file, path_virtual):
     # if len(sys.argv) < 4:
     #     raise ValueError("Usage: python make_vcapp.py A.UPF B.UPF <x value [0:1] of Ax+B(1-x)>")
     # ppa=Path(sys.argv[1])
@@ -141,7 +142,7 @@ def main(ppa,ppb,x, dest, virtual, precision, location):
     #     dest=sys.argv[4]
     # except:
     #     dest="./"
-    make_vcapp(ppa, ppb, x, dest=dest, path_virtual=virtual, precision=precision, location=location)
+    make_vcapp(ppa, ppb, x, fname=file, path_virtual=path_virtual)
 
 if __name__ == "__main__":
     main()
